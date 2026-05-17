92fc0472f3e459a1f61895142f689fbf4c72591e
khanhphamejmfq-ui <khanhphamejmfq@gmail.com>
2026-05-17 13:43:45 +0800

    disable update notification popup completely

--- components/UpdateNotificationEvent.tsx
+++ components/UpdateNotificationEvent.tsx
@@ -1,6 +1,9 @@
-export const UPDATE_NOTIFICATION_KEY = 'xiaoxingos_update_2026_04_seen';
-export const UPDATE_NOTIFICATION_KEY_2026_05 = 'xiaoxingos_update_2026_05_seen';
-export const UPDATE_NOTIFICATION_KEY_2026_05_10 = 'xiaoxingos_update_2026_05_10_seen';
+import React from 'react';
+
+export const UPDATE_NOTIFICATION_KEY = 'xiaoxingos_update_seen_disabled';
+export const UPDATE_NOTIFICATION_KEY_2026_05 = 'xiaoxingos_update_seen_disabled_2026_05';
+export const UPDATE_NOTIFICATION_KEY_2026_05_10 = 'xiaoxingos_update_seen_disabled_2026_05_10';
 export const FAQ_TARGET_SECTION_KEY = 'xiaoxingos_faq_target_section';
-export const UpdateNotificationEvent = () => null;
+
+export const UpdateNotificationEvent: React.FC<any> = () => null;
 export default UpdateNotificationEvent;
