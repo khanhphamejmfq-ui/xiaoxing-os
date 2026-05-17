1294a5fdec96869475d7f658b5f97c44cbbedfd5
khanhphamejmfq-ui <khanhphamejmfq@gmail.com>
2026-05-17 13:47:20 +0800

    disable update notification component completely

--- components/UpdateNotificationEvent.tsx
+++ components/UpdateNotificationEvent.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const UPDATE_NOTIFICATION_KEY = 'xiaoxingos_update_seen_disabled';
-export const UPDATE_NOTIFICATION_KEY_2026_05 = 'xiaoxingos_update_seen_disabled_2026_05';
-export const UPDATE_NOTIFICATION_KEY_2026_05_10 = 'xiaoxingos_update_seen_disabled_2026_05_10';
+export const UPDATE_NOTIFICATION_KEY = 'xiaoxingos_update_seen';
+export const UPDATE_NOTIFICATION_KEY_2026_05 = 'xiaoxingos_update_2026_05_seen';
+export const UPDATE_NOTIFICATION_KEY_2026_05_10 = 'xiaoxingos_update_2026_05_10_seen';
 export const FAQ_TARGET_SECTION_KEY = 'xiaoxingos_faq_target_section';
 
 export const UpdateNotificationEvent: React.FC<any> = () => null;
